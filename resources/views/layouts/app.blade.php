ef71e496fdd4cdd966e8ab2baf2d3398d1e7b763
Elbek Khamdullaev <elbek.khamdullaev@gmail.com>
2024-01-09 19:06:27 +0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,29 @@
 </head>
 
 <body>
+<!-- ======= Header ======= -->
+    <header id="header" class="fixed-top ">
+        <div class="container d-flex align-items-center">
 
+            <h1 class="logo me-auto"><a href="{{url('/')}}">Khamdullaev.uz</a></h1>
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li><a class="nav-link scrollto active" href="#hero">Bosh sahifa</a></li>
+                    <li><a class="nav-link scrollto" href="#about">Men haqimda</a></li>
+                    <li><a class="nav-link scrollto" href="#services">Xizmatlar</a></li>
+                    <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
+                    <li><a class="nav-link scrollto" href="#pricing">Narxlar</a></li>
+                    <li><a class="nav-link scrollto" href="#contact">Bog'lanish</a></li>
+                    <li><a class="getstarted scrollto" href="#contact">Buyurtma berish</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+
+        </div>
+    </header><!-- End Header -->
 @yield('content')
 <!-- ======= Footer ======= -->
 <footer id="footer">
